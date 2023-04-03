c00834ef564cfe7675e6a1251fce21c3e110638e
TheOdd23 <100093373+TheOdd23@users.noreply.github.com>
2023-04-03 14:26:08 -0400

    Update get_next_line.c

--- srcs/get_next_line.c
+++ srcs/get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "../includes/get_next_line.h"
 
 void	ft_bzero(void *str, size_t n)
 {
